6c2f3aa2db3df79d59b88815d40c723a9b0dec06
shirish71 <shirishmaharjan71@gmail.com>
2021-01-31 16:45:22 +0545

    add profile

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,6 @@
 Route::group(['namespace' => 'Shirish71\AdminPanel\Http\Controllers','as','admin.','prefix'=>'admin'], function () {
     Route::get('dashboard', 'DashboardController@index');
     Route::get('profile', 'DashboardController@index')->name('admin.profile.index');
+    Route::get('profile', 'DashboardController@index')->name('admin.profile.index');
     Route::post('logout', 'DashboardController@index')->name('admin.logout');
 });
